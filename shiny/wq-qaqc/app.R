347bb677a4aacdeb649b4aa9153bbe067b2af16e
Dunnigan <Shannon.Dunnigan@dep.state.fl.us>
2022-12-08 16:48:36 -0500

    start shiny development

---
+++ shiny/wq-qaqc/app.R
@@ -0,0 +1,54 @@
+## app.R ##
+
+# install.packages(c('shinydashboard', 'shiny'))
+
+library(shinydashboard)
+library(shiny)
+library(dygraphs)
+
+# ui
+
+ui <- dashboardPage(
+  dashboardHeader(title = "SWMP Water Quality"),
+  dashboardSidebar(
+    sidebarMenu(
+    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+    menuItem("Widgets", icon = icon("th"), tabName = "widgets",
+             badgeLabel = "new", badgeColor = "green")
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      # First tab content
+      tabItem(tabName = "dashboard",
+              fluidRow(
+                box(plotOutput("plot1", height = 250)),
+                
+                box(
+                  title = "Controls",
+                  sliderInput("slider", "Number of observations:", 1, 100, 50)
+                )
+              )
+      ),
+      
+      # Second tab content
+      tabItem(tabName = "widgets",
+              h2("Widgets tab content")
+      )
+    )
+  )
+)
+
+# server
+
+server <- function(input, output){
+  set.seed(122)
+  histdata <- rnorm(500)
+  
+  output$plot1 <- renderPlot({
+    data <- histdata[seq_len(input$slider)]
+    hist(data)
+  })
+}
+
+shinyApp(ui, server)
